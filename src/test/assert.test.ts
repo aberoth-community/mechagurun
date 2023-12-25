e744b414c9fe92676c7deb3da1740cee9a7b3976
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2023-12-24 20:41:28 -0800

    test: util/assert

--- src/test/assert.test.ts
+++ src/test/assert.test.ts
@@ -1,6 +1,14 @@
-import { describe, test } from 'vitest'
+import { expect, describe, test } from 'vitest'
+import { assert } from '../util/assert'
 
 describe('assert', () => {
-  test.skip('assert should throw')
-  test.skip('assert should return input')
+  test('assert should throw', () => {
+    expect(() => {
+      assert(null, 'testing')
+    }).toThrowError(/testing/)
+  })
+
+  test('assert should return input', () => {
+    expect(assert(12, 'testing')).toBe(12)
+  })
 })
